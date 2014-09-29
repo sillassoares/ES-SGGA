198853678b54f82e02686bee182dd5acd1bf3713
vitorbrando <vitorbrando@gmail.com>
2014-09-28 22:15:28 -0300

    Commit inicial do projeto SGGA

---
+++ src/sgga/carro.java
@@ -0,0 +1,16 @@
+package sgga;
+
+public class carro {
+	
+	private String nome;
+	
+	public String getNome()
+	{
+		return nome;
+	}
+	
+	public void setNome(String nome)
+	{
+		this.nome = nome;
+	}
+}
---
+++ src/sgga/login.java
@@ -0,0 +1,49 @@
+package sgga;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class login
+ */
+@WebServlet("/login")
+public class login extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public login() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String usuario = request.getParameter("usuario");
+		String senha = request.getParameter("senha");
+		
+		if ((usuario.equals("vitor")) && (senha.equals("1234")))
+		{
+			response.sendRedirect("success.jsp");
+		}
+		else
+		{
+			response.sendRedirect("error.jsp");		
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
